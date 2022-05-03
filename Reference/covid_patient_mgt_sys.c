5289740b12970029ae118a3e150e2f9d390d6d56
r-e-d-ant <mugishathierry65@gmail.com>
2022-05-03 21:38:00 +0200

    some changes

--- Reference/covid_patient_mgt_sys.c
+++ Reference/covid_patient_mgt_sys.c
@@ -1,23 +1,36 @@
 
+
 #include<stdio.h>
 #include<string.h>
+
+// =================== 
+
+/* Functions declation
+there are not necessary if the functions are at the top of the main function (which is the case)
+so You can remove them if you want */
 
 void recordPatient();
 void displayPatients();
 void deletePatient();
 
+// ===================== 
+
+/*
+Structure Patient declare as array of 5 length
+*/
 struct Patient
 {
 	char name[20], sex[10];
 	int age;
 } p[5];
 
+// tot variable to keep track of the amount of the patient
 int tot = 0;
 
 // record Patient function
 void recordPatient()
 {
-	printf("\nEnter: \n");
+	printf("\nEnter \n");
 
 	printf("\nName: ");
 	scanf("%s", p[tot].name);
@@ -28,7 +41,7 @@ void recordPatient()
 	printf("\nSex: ");
 	scanf("%s", p[tot].sex);
 
-	tot++;
+	tot++; // increase the amount of the patient when one patient is added
 }
 
 // Display all patients function
@@ -38,30 +51,40 @@ void displayPatients()
 	int i;
 
 	printf("\nPatients records:\n");
+	printf("\nName\tAge\tSex\n====\t===\t===\n");
 
 	for(i = 0; i < tot; i++) {
-		printf("\nName: %s\n", p[i].name);
-		printf("\nAge: %d\n", p[i].age);
-		printf("\nSex: %s\n", p[i].sex);
+		printf("%s\t%d\t%s\n", p[i].name, p[i].age, p[i].sex);
 	}
 }
 
 // Update patient function
+/*
+In this function there is new functions (pName, newSex, and newAge) to store
+entered new data of the patient and we will use them to replace the ones which are
+already entered.
+
+1st we check if the name is available
+if not we pass 1 to found variable
+if the patient is available
+we we will display his datas then update them
+*/
 void updatePatient()
 {
 	char pName[20], newSex[10];
-	int i, newAge;
+	int i, newAge, found=0;
 
 	printf("Enter patient Name: ");
 	scanf("%s", pName);
 
 	for(i = 0; i < tot; i++) {
 		if(strcmp(p[i].name, pName) == 0) {
+			found = 1; // we pass one when the name is found
+
 			printf("\nOld data: \n");
 
-			printf("Name: %s\n", p[i].name);
-			printf("Age: %d\n", p[i].age);
-			printf("Sex: %s\n", p[i].sex);
+			printf("\nName\tAge\tSex\n====\t===\t===\n");
+			printf("%s\t%d\t%s\n", p[i].name, p[i].age, p[i].sex);
 
 			// inserting new data
 			printf("\nEnter new: \n");
@@ -84,32 +107,60 @@ void updatePatient()
 			// replace entered sex
 			strcpy(p[i].sex, newSex);
 
-		} else {
-			printf("Name %s not found!", pName);
 		}
+	}
+	// if found is still zero we tell the user that the name is not available
+	if(found == 0) {
+		printf("Patient with name %s not found! Try again", pName);
 	}
 }
 
-// Delete patient function
-void deletePatient()
+// Search one patient function
+void searchPatient()
 {
 	char pName[20];
-	int i;
+	int i, found=0;
 
 	printf("Enter patient Name: ");
 	scanf("%s", pName);
 
 	for(i = 0; i < tot; i++) {
 		if(strcmp(p[i].name, pName) == 0) {
-			printf("\nPatient data: \n");
+			found = 1; // we pass one when the name is found
 
-			printf("Name: %s\n", p[i].name);
-			printf("Age: %d\n", p[i].age);
-			printf("Sex: %s\n", p[i].sex);
-
-		} else {
-			printf("Name %s not found!", pName);
+			printf("\nName\t\tAge\tSex\n====\t\t===\t===\n");
+			printf("%s\t\t%d\t%s\n", p[i].name, p[i].age, p[i].sex);
 		}
+	}
+
+	// if found is still zero we tell the user that the name is not available
+	if(found == 0) {
+		printf("Patient with name %s not found! Try again", pName);
+	}
+
+}
+
+// Delete patient function
+// Still don't know how to delete
+void deletePatient()
+{
+	char pName[20];
+	int i, found=0;
+
+	printf("Enter patient Name: ");
+	scanf("%s", pName);
+
+	for(i = 0; i < tot; i++) {
+		if(strcmp(p[i].name, pName) == 0) {
+			found = 1;
+			printf("\nOops!\n");
+			printf("The patient is inside and the program can't remove him/her\n");
+
+		}
+	}
+
+	if(found == 0) {
+		printf("Patient with name %s not found! Try again", pName);
 	}
 
 }
@@ -122,7 +173,7 @@ int main()
 
 	do
 	{
-		printf("\nEnter\n1. Record\n2. Display\n3. Update\n4. Delete\n0. Exit\n: > ");
+		printf("\nEnter:\n[1]. Record\n[2]. Display\n[3]. Update\n[4]. Search\n[5]. Delete\n[0]. Exit\n: > ");
 		scanf("%d", &c);
 		switch(c)
 		{
@@ -136,13 +187,16 @@ int main()
 				updatePatient();
 				break;
 			case 4:
+				searchPatient();
+				break;
+			case 5:
 				deletePatient();
 				break;
 			case 0:
 				printf("\nThank you!");
 				break;
 			default:
-				printf("\nChoice must be 0, 1, 2, 3, or 4.");
+				printf("\nChoice must be 0, 1, 2, 3, 4, or 5.");
 		}
 	} while(c != 0);
 
--- main.c
+++ main.c
@@ -1,2 +1,223 @@
 
+
 #include<stdio.h>
+#include<string.h>
+
+// =================== 
+
+/* Functions declation
+there are not necessary if the functions are at the top of the main function (which is the case)
+so You can remove them if you want */
+
+void recordPatient();
+void displayPatients();
+void updatePatient();
+void searchPatient();
+void deletePatient();
+
+// ===================== 
+
+/*
+Structure Patient declare as array of 5 length
+*/
+struct Patient
+{
+	char name[30], date[20], email[30];
+	int phone_no;
+} p[5];
+
+// tot variable to keep track of the amount of the patient
+int tot = 0;
+
+// Main function
+int main()
+{
+	int c;
+	c = 0;
+
+	do
+	{
+		printf("\nEnter:\n[1]. Record\n[2]. Display\n[3]. Update\n[4]. Search\n[5]. Delete\n[0]. Exit\n: > ");
+		scanf("%d", &c);
+		switch(c)
+		{
+			case 1:
+				recordPatient();
+				break;
+			case 2:
+				displayPatients();
+				break;
+			case 3:
+				updatePatient();
+				break;
+			case 4:
+				searchPatient();
+				break;
+			case 5:
+				deletePatient();
+				break;
+			case 0:
+				printf("\nThank you!");
+				break;
+			default:
+				printf("\nChoice must be 0, 1, 2, 3, 4, or 5.");
+		}
+	} while(c != 0);
+
+	printf("\n");
+}
+
+
+
+// record Patient function
+void recordPatient()
+{
+	printf("\nEnter \n");
+
+	printf("\nName: ");
+	scanf("%s", p[tot].name);
+
+	printf("\nDate (format: [dd/mm/22]): ");
+	scanf("%s", p[tot].date);
+
+	printf("\nEmail: ");
+	scanf("%s", p[tot].email);
+
+	printf("\nPhone no (ex: 25078xxxxxx): ");
+	scanf("%d", &p[tot].phone_no);
+
+	tot++; // increase the amount of the patient when one patient is added
+}
+
+// Display all patients function
+void displayPatients()
+{
+
+	int i;
+
+	printf("\nPatients records:\n");
+	printf("\nName\t\t\t\tDate\t\t\tEmail\t\t\t\tPhone\n=======\t\t\t\t======\t\t\t=======\t\t\t\t========\n");
+
+	for(i = 0; i < tot; i++) {
+		printf("%s\t\t\t\t%s\t\t\t%s\t\t\t\t%d\n", p[i].name, p[i].date, p[i].email, p[i].phone_no);
+	}
+}
+
+// Update patient function
+/*
+In this function there is new functions (pName, newSex, and newAge) to store
+entered new data of the patient and we will use them to replace the ones which are
+already entered.
+
+1st we check if the name is available
+if not we pass 1 to found variable
+if the patient is available
+we we will display his datas then update them
+*/
+void updatePatient()
+{
+	char pName[30], newDate[20], newEmail[30];
+	int i, newPhone_no, found=0;
+
+	printf("Enter patient Name: ");
+	scanf("%s", pName);
+
+	for(i = 0; i < tot; i++) {
+		if(strcmp(p[i].name, pName) == 0) {
+			found = 1; // we pass one when the name is found
+
+			printf("\nOld data: \n");
+
+			printf("\nName\t\t\t\tDate\t\t\tEmail\t\t\t\tPhone\n=======\t\t\t\t======\t\t\t=======\t\t\t\t========\n");
+			printf("%s\t\t\t\t%s\t\t\t%s\t\t\t\t%d\n", p[i].name, p[i].date, p[i].email, p[i].phone_no);
+
+			// inserting new data
+			printf("\nEnter new \n");
+
+			printf("\nName: ");
+			scanf("%s", pName);
+			// replace entered name
+			strcpy(p[i].name, pName);
+
+			// ------====------------
+
+			printf("\nDate (format: [dd/mm/22]): ");
+			scanf("%s", newDate);
+			// replace entered Date
+			strcpy(p[i].date, newDate);
+
+			// ------====------------
+
+			printf("\nEmail: ");
+			scanf("%s", newEmail);
+			// replace entered email
+			strcpy(p[i].email, newEmail);
+
+			// ------====------------
+
+			printf("\nPhone no (ex: 25078xxxxxx): ");
+			scanf("%d", &newPhone_no);
+			// replace entered new phone number
+			p[i].phone_no = newPhone_no;
+
+		}
+	}
+	// if found is still zero we tell the user that the name is not available
+	if(found == 0) {
+		printf("Patient with name %s not found! Try again", pName);
+	}
+}
+
+// Search one patient function
+void searchPatient()
+{
+	char pName[20];
+	int i, found=0;
+
+	printf("Enter patient Name: ");
+	scanf("%s", pName);
+
+	for(i = 0; i < tot; i++) {
+		if(strcmp(p[i].name, pName) == 0) {
+			found = 1; // we pass one when the name is found
+
+			printf("\nName\t\t\t\tDate\t\t\tEmail\t\t\t\tPhone\n=======\t\t\t\t======\t\t\t=======\t\t\t\t========\n");
+			printf("%s\t\t\t\t%s\t\t\t%s\t\t\t\t%d\n", p[i].name, p[i].date, p[i].email, p[i].phone_no);
+		}
+	}
+
+	// if found is still zero we tell the user that the name is not available
+	if(found == 0) {
+		printf("Patient with name %s not found! Try again", pName);
+	}
+
+}
+
+// Delete patient function
+// Still don't know how to delete
+void deletePatient()
+{
+	char pName[20];
+	int i, found=0;
+
+	printf("Enter patient Name: ");
+	scanf("%s", pName);
+
+	for(i = 0; i < tot; i++) {
+		if(strcmp(p[i].name, pName) == 0) {
+			found = 1;
+			printf("\nOops!\n");
+			printf("The patient is inside and the program can't remove him/her\n");
+
+		}
+	}
+
+	if(found == 0) {
+		printf("Patient with name %s not found! Try again", pName);
+	}
+
+}
+
+
+
+
